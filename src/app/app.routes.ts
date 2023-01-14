d38f20e66ef0f48c561a415b810ce4da6c1be36b
João Paulo <mrjoao16@gmail.com>
2023-01-14 16:09:30 -0300

    Segunda Parte

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from "@angular/router";
 
-export const AppRoutes: Routes =[
+export const AppRotas: Routes =[
   {
-
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) //LoginComponent
   }
 ]
